ed4ebd5d0d40ffee963aa4ceeae1813c97dc9844
hanifah-02 <158190105+hanifah-02@users.noreply.github.com>
2024-02-01 07:04:04 -0800

    Add files via upload

---
+++ questionaire.py
@@ -0,0 +1,14 @@
+#effective customer behaviour research
+name = input("what is your name;")
+
+question_1 = input("who purchases your products and services;")
+
+question_2 = input("who makes the buying decisons;")
+
+question_3 = input("where are your products purchased;")
+
+question_4 = input("which kind of products do you purchase;")
+
+question_5 = input("what period do u take until you purchase other goods;")
+
+print(f"Hello Mr.{name} your products and services are purchased by {question_1} and {question_2} make the buying decisions {question_3} is where you purchase your products so {question_4} are the products purchased{question_5} is the period taken until other goods are purchased")
